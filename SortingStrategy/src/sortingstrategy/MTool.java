1cc5f9e0a8506078f4414bc83409ab81328a51bd
Gregory Gilbert <gergtreblig@hotmail.com>
2016-09-27 12:13:50 -0600

    Update MTool.java

--- SortingStrategy/src/sortingstrategy/MTool.java
+++ SortingStrategy/src/sortingstrategy/MTool.java
@@ -11,9 +11,43 @@ package sortingstrategy;
  */
 public class MTool implements Sorter {
     
-    public int[] mathSort(int[] arr){
-        // Merg sort algo goes here
-        return arr;
-    }
-    
+    public int[] mathSort(int[] array){
+		if(list.length <= 1){
+			return list;
+		}
+		int[] front = new int[array.length/2];
+		int[] back = new int[array.length-front.length];
+		for(int i = 0; i < array.length; i++){
+			if(i < front.length){
+				front[i] = array[i];
+			}
+			else{
+				back[i-front.length] = array[i];
+			}
+		}
+		mathSort(front);
+		mathSort(back);
+
+		for(int i = 0; i < array.length; i+=0){
+			for(int j = 0; j < front.length; j+=0){
+				for(int k = 0; k < back.length; k+=0){
+					if(front[j]<back[k]){
+						array[i] = front[j];
+						i++;
+						if(j+1 < front.length){
+							j++;
+						}
+					}
+					else{
+						array[i] = back[k];
+						i++;
+						if(k+1 < back.length){
+							k++;
+						}
+					}
+				}
+			}
+		}
+		return array;
+	}
 }
