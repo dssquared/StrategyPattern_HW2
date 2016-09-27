5c2278398eea6474a87151bcfeccd11279ff75c8
Gregory Gilbert <gergtreblig@hotmail.com>
2016-09-27 12:00:39 -0600

    Update Mathematica.java

--- SortingStrategy/src/sortingstrategy/Mathematica.java
+++ SortingStrategy/src/sortingstrategy/Mathematica.java
@@ -9,11 +9,19 @@ package sortingstrategy;
  *
  * @author David Schwehr <dpschwehr[AT]gmail[dotCOM]> github: dssquared
  */
-public class Mathematica implements Sorter {
-    
-    public int[] mathSort(int[] arr){
-        //  insertion sort algo goes here
-        return arr;
-    }
-    
-}  // end class
+public class Mathematica implements Sorter{
+	public int[] mathSort(int[] array){
+		for(i = 0; i < array.length; i++){
+			int smallestIndex = i;
+			for(j = 0; j < array.length; j++){
+				if(array[j] < array[smallestIndex]){
+					smallestIndex = j;
+				}
+			}
+			int smallestNum = array[smallestIndex];
+			array[smallestIndex] = array[i];
+			array[i] = smallestNum;
+		}
+		return array;
+	}
+}
